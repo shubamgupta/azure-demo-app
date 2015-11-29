c89748c7378eafb8ba91baffb52fd6be1f516d5d
Ben Navetta <ben.navetta@gmail.com>
2015-11-28 23:54:08 -0500

    try file logging

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -2,9 +2,16 @@
 The flask application package.
 """
 
+import logging
+from logging.handlers import FileHandler
+
 from flask import Flask
 app = Flask(__name__)
 
 app.debug = True
+
+handler = FileHandler('wsgi.log')
+handler.setLevel(logging.DEBUG)
+app.logger.addHandler(handler)
 
 import FlaskWebProject.views
