6ffdaf0523e05631e777ede22b213430f00b8a0e
Ben Navetta <ben.navetta@gmail.com>
2015-11-29 21:08:36 -0500

    just use wsgi logging

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -2,16 +2,16 @@
 The flask application package.
 """
 
-import logging
-from logging.handlers import FileHandler
+# import logging
+# from logging.handlers import FileHandler
 
 from flask import Flask
 app = Flask(__name__)
 
 app.debug = True
 
-handler = FileHandler('D:\home\site\wwwroot\app.log')
-handler.setLevel(logging.DEBUG)
-app.logger.addHandler(handler)
+# handler = FileHandler('D:\home\site\wwwroot\app.log')
+# handler.setLevel(logging.DEBUG)
+# app.logger.addHandler(handler)
 
 import FlaskWebProject.views
